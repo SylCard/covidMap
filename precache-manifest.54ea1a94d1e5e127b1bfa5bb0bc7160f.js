5496e96798142213dd16741d932dc7e426fd497b
Sylvester <sbcardorelle@hotmail.co.uk>
2020-04-27 23:00:07 +0100

    Updates

---
+++ precache-manifest.54ea1a94d1e5e127b1bfa5bb0bc7160f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "145f844421dec6053389dee352a8ad7f",
+    "url": "/covidMap/index.html"
+  },
+  {
+    "revision": "228e276d63f92306a799",
+    "url": "/covidMap/static/css/main.8fe42b9b.chunk.css"
+  },
+  {
+    "revision": "cf0416e95707d72f4de7",
+    "url": "/covidMap/static/js/2.967bf19f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covidMap/static/js/2.967bf19f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "228e276d63f92306a799",
+    "url": "/covidMap/static/js/main.f37ce878.chunk.js"
+  },
+  {
+    "revision": "10da2eb7c07fc9ea089d",
+    "url": "/covidMap/static/js/runtime-main.e49910c9.js"
+  }
+]);
